8c9f49a432d56e2c5c5870a12cf846e3518c6afa
Andrew Wright <naruhodo007@gmail.com>
2018-11-20 13:11:02 +0000

    Added users and now need to build modules/apis

---
+++ travel_estimator/users/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
+class CustomUserManager(UserManager):
+    pass
+
+
+class CustomUser(AbstractUser):
+    objects = CustomUserManager()
+    
+
+
